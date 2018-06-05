67bf4fb0e6cf94697eea36ac7d1b2dfd8adf09df
Gabriel Gomez <gleandro@ceres.solutions>
2018-06-05 11:05:23 -0300

    Initial commit

---
+++ spec/graphite-sh-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GraphiteShView from '../lib/graphite-sh-view';
+
+describe('GraphiteShView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
